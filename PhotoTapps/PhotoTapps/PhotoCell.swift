955d214632ab6db81df81ea3af452583cfa78bc5
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-06-09 12:41:49 +0200

    create file

---
+++ PhotoTapps/PhotoTapps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by serg on 09.06.2020.
+//  Copyright © 2020 serg. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
