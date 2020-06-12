7b797864a76b37057beb2ca296b19760cea3fafa
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-06-12 14:38:06 +0200

    first commit

--- ViewControllerScene/ViewControllerScene/ViewController.swift
+++ ViewControllerScene/ViewControllerScene/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textColor = UIColor.red
+        button.setTitle("Get result", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .green
+        
     }
 
-
+    @IBAction func pressedButton(_ sender: UIButton) {
+        if label.isHidden{
+            label.isHidden = false
+            label.text = "Hello, World!"
+            button.setTitle("Clear", for: UIControl.State.normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .red
+        } else {
+            label.isHidden = true
+            button.setTitle("Get result", for: .normal)
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+    }
+    
 }
 
